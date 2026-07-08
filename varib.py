cf0738a9be55bee4da644555414b7b5840ffbeed
priyagowdam06-bit <priyagowdam06@gmail.com>
2026-07-08 19:59:28 +0530

    new commit

---
+++ varib.py
@@ -0,0 +1,22 @@
+####variable+datatypes############
+username = "priya"
+followers = 10000
+verified = True
+rating = 4.5
+
+print(username)
+print(followers)
+print(verified)
+
+print(rating)
+########################new##############################
+username = input("enter the username")
+followers = int(input("enter followers count"))
+verified = input("is the account verified(True/False):")=="True"
+rating = float(input("enter rating"))
+
+print("username:",username)
+print("followers:",followers)
+print("verified:",verified)
+print("rating:",rating)
+

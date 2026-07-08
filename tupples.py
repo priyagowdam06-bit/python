9bf9ed7bcccfb08c932d989b47c2edb67deb2791
priyagowdam06-bit <priyagowdam06@gmail.com>
2026-07-08 10:56:56 +0530

    new commit

---
+++ tupples.py
@@ -0,0 +1,54 @@
+# students = ("ram","sam","bheem")
+# print(students[-2])
+# #########tuple is a collection used to store multiple
+# numbers = (10,20,30,40)
+# print(numbers [-3])
+
+# data = (1,2,3)
+# data[0] = 100
+
+# print(data)
+# x=(1,2,3,2,1,1,1)
+# print(x.count(1))
+# print(x.count(2))
+
+# x = ("apple","banana","grapes","banana")
+# print(x.count("banana"))
+
+# num = (10,20,30,40,50)
+# print(num[1:4])
+
+# ###########sets#############
+# ##set is a collection of : remove duplicate values
+
+# x = {1,2,3,2,1,1,1}
+# print(x)
+# data = {1,2,3}
+# data.add(4)
+# print(data)
+
+#########Function############
+
+def greeting():
+    print("hello students")
+greeting()
+
+def add():
+    return 10 + 20
+result = add()
+print(result)
+
+def sub():
+    return 20 - 10
+result = sub()
+print(result)
+
+def mul():
+    return 2 * 5
+result = mul()
+print(result)
+
+def div():
+    return 8 / 2
+result = div()
+print(result)
